7bad1bab718f0b0eac67f86c8382ceb70f8a1689
Koen Vos <koenvos@users.noreply.github.com>
2016-04-14 01:11:20 +0800

    basic zooming

--- vocle.m
+++ vocle.m
@@ -4,15 +4,28 @@ function vocle(varargin)
 % - same basic interaction as spclab
 % - save configuration such as window location and size, and zoom between calls to vocle
 % - checkboxes next to samples
+% - drop down menu for sampling rate (and remember)
 % - A/B test
 % - stereo support
-% - different sampling rates
 % - start/stop playback
+% - different sampling rates per sample?
 
 % settings
 fig_no = 9372;
 config_file = [which('vocle'), 'at'];
 axes_label_font_size = 8;
+% axes
+left_margin = 42;
+%right_margin = 32;
+right_margin = 20;
+bottom_margin = 80;
+top_margin = 16;
+vert_spacing = 28;
+%checkbox_size = 20;
+
+time_range_full = [0, 0];
+time_range_view = [0, 0];
+zoom_range = [0, 0];
 
 
 % detect if figure exists
@@ -33,57 +46,124 @@ if ~fig_exist
         load_config();
     end
 end
+h_width = h.Position(3);
+h_height = h.Position(4);
 write_config();
+
+% process signals
+num_signals = length(varargin);
+signals = cell(num_signals, 1);
+signal_lenghts = zeros(num_signals, 1);
+for k = 1:num_signals
+    sz = size(varargin{k});
+    if sz(1) > sz(2)
+        signals{k} = varargin{k};
+    else
+        signals{k} = varargin{k}';
+    end
+    signal_lenghts(k) = max(sz);
+end
+time_range_full = [1, max(signal_lenghts)];
 
 % create axes and checkboxes, one per input array/matrix
 clf;
-num_axes = length(varargin);
-h_ax = cell(num_axes, 1);
-h_cb = cell(num_axes, 1);
-for k = 1:num_axes
+h_ax = cell(num_signals, 1);
+%h_cb = cell(num_signals, 1);
+for k = 1:num_signals
     h_ax{k} = axes;
-    plot(varargin{k});
+    plot(signals{k}, 'HitTest', 'off');
     h_ax{k}.UserData = k;
     h_ax{k}.FontSize = axes_label_font_size;
     h_ax{k}.Units = 'pixels';
-    h_ax{k}.ButtonDownFcn = {@axes_button_down_callback};
-    h_cb{k} = uicontrol('Style', 'checkbox');
-    h_cb{k}.UserData = k;
-    h_cb{k}.Callback = {@axes_checkbox_callback};
+    h_ax{k}.ButtonDownFcn = @axes_button_down_callback;
+    %h_cb{k} = uicontrol('Style', 'checkbox');
+    %h_cb{k}.UserData = k;
+    %h_cb{k}.Callback = @axes_checkbox_callback;
 end
 axes_layout;
+time_range_view = time_range_full;
+plot_zoom;
 
 % set figure callbacks
-h.CloseRequestFcn = {@window_close_callback};
-h.SizeChangedFcn = {@window_resize_callback};
+h.CloseRequestFcn = @window_close_callback;
+h.SizeChangedFcn = @window_resize_callback;
+h.ButtonDownFcn = @window_button_down_callback;
+h.WindowButtonUpFcn = '';
+
+    function plot_zoom
+        for k = 1:num_signals
+            s = signals{k};
+            if time_range_view(1) <= signal_lenghts(k)
+                s_ = s(max(round(time_range_view(1)), 1) : min(round(time_range_view(2)), end));
+                maxy = 1.1 * max(abs(s_));
+            else
+                maxy = 1;
+            end
+            if min(s) >= 0
+                miny = 0;
+            else
+                miny = -maxy;
+            end
+            h_ax{k}.XLim = time_range_view;
+            h_ax{k}.YLim = [miny, maxy];
+        end
+    end
+        
+
+
+%     function button_down_callback(src, evt)
+%         disp('bd')
+%         gca.UserData
+%     end
 
     % distribute axes over figure
     function axes_layout()
         h_width = h.Position(3);
         h_height = h.Position(4);
-        left_margin = 40;
-        right_margin = 32;
-        bottom_margin = 80;
-        top_margin = 8;
-        vert_spacing = 25;
-        cb_size = 20;
-        hght = (h_height - top_margin - bottom_margin - (num_axes-1) * vert_spacing) / num_axes;
+        hght = (h_height - top_margin - bottom_margin - (num_signals-1) * vert_spacing) / num_signals;
         width = h_width - left_margin - right_margin;
         if hght > 0 && width > 0
-            for kh = 1:num_axes
-                bottom = bottom_margin + (num_axes-kh) * (hght + vert_spacing);
+            for kh = 1:num_signals
+                bottom = bottom_margin + (num_signals-kh) * (hght + vert_spacing);
                 h_ax{kh}.Position = [left_margin, bottom, width, hght];
-                h_cb{kh}.Position = [left_margin + width + 8, bottom + hght/2 - cb_size*0.77, cb_size, 30];
+                %h_cb{kh}.Position = [left_margin + width + 8, bottom + hght/2 - checkbox_size*0.77, checkbox_size, 30];
             end
         end
     end
 
     function axes_button_down_callback(src, evt)
         disp(['button down on axes ', num2str(src.UserData)])
+        switch(h.SelectionType)
+            case 'normal'
+                % either select or mark
+                zoom_range(1) = min(max((h.CurrentPoint(1) - left_margin) / (h_width - left_margin - right_margin), 0), 1);
+                h.WindowButtonUpFcn = @button_up_callback;
+            case 'alt'
+                % zoom out 2x
+                time_range_view = 0.5 * sum(time_range_view) + diff(time_range_view) * [-1 1];
+                time_range_view(1) = max(time_range_full(1), time_range_view(1));
+                time_range_view(2) = min(time_range_full(2), time_range_view(2));
+                plot_zoom;
+            case 'open'
+                % zoom out full
+                time_range_view = time_range_full;
+                plot_zoom;
+        end
+    end
+    function button_up_callback(src, evt)
+        disp(['button up on axes ', num2str(src.UserData)])
+        zoom_range(2) = min(max((h.CurrentPoint(1) - left_margin) / (h_width - left_margin - right_margin), 0), 1);
+        time_range_view = sort(time_range_view(1) + zoom_range * diff(time_range_view));
+        plot_zoom;
+        h.WindowButtonUpFcn = '';
     end
 
-    function axes_checkbox_callback(src, evt)
-        disp(['checkbox ', num2str(src.UserData)])
+%     function axes_checkbox_callback(src, evt)
+%         disp(['checkbox ', num2str(src.UserData)])
+%     end
+
+    function window_button_down_callback(src, evt)
+        % unselect all axes
     end
 
     function window_resize_callback(src, evt)
