6d291c89541f648c21b6a33694be885bd4937ff3
Koen Vos <koenvos@hotmail.com>
2016-04-15 16:44:28 +0800

    refactoring to better deal with changes in sampling rate and long signals

--- vocle.m
+++ vocle.m
@@ -1,18 +1,16 @@
 function vocle(varargin)
 % audio navigator
-% ideas:
-% - same basic interaction as spclab
-
+% 
+% same basic interaction as spclab
 % advantages over spclab:
 % - save configuration such as window location, samping rate, and zoom between calls to vocle
-% - A/B test
 % - scroll wheel zooming
 % - stereo support
+% - A/B test
 % - start/stop playback
 % - different sampling rates per sample?
 
 % todo:
-% - clearer selected axis indication: bolder outline line?
 % - show segment time duration in top left corner
 % - play
 % - stop
@@ -31,23 +29,12 @@ bottom_margin = 90;
 top_margin = 16;
 vert_spacing = 27;
 slider_height = 16;
-%figure_color = [0.935, 0.939, 0.94];
 figure_color = [0.915, 0.918, 0.92];
 selection_color = [0.952, 0.956, 0.96];
-%selection_color = [0.94, 0.945, 0.94];
 segment_color = [0.88, 0.92, 0.96];
-zoom_per_scroll_wheel_step = 1.3;
+zoom_per_scroll_wheel_step = 1.4;
 ylim_margin = 1.1;
 fs = 48000;  % default; overwritten if config file exists
-
-% function-wide variables
-h_ax = [];
-selected_axes = [];
-time_range_full = [0, 0];
-time_range_view = [0, 0];
-last_button_down = [];
-%cursor_line = [];
-segment_patch = [];
 
 % create figure if doesn't exist yet and sync with config file
 h = figure(fig_no);
@@ -63,11 +50,12 @@ load_config;
 % process signals
 num_signals = length(varargin);
 signals = cell(num_signals, 1);
-signal_times = zeros(num_signals, 1);
+signal_lengths = zeros(num_signals, 1);
 signals_negative = zeros(num_signals, 1);
 signals_ylim = zeros(num_signals, 1);
 for k = 1:num_signals
     sz = size(varargin{k});
+    signal_lengths(k) = max(sz);
     if sz(1) > sz(2)
         signals{k} = varargin{k};
     else
@@ -78,6 +66,15 @@ for k = 1:num_signals
     signals_ylim(k) = ylim_margin * max(abs(s_));
 end
 
+% function-wide variables
+h_ax = [];
+selected_axes = [];
+time_range_view = [];
+last_button_down = [];
+%cursor_line = [];
+segment_range = [];
+segment_patches = cell(num_signals, 1);
+
 % put elements on UI
 clf;
 h_ax = cell(num_signals, 1);
@@ -86,7 +83,7 @@ for k = 1:num_signals
     h_ax{k}.Units = 'pixels';
 end
 time_slider = uicontrol(h, 'Style', 'slider', 'Value', 0.5, ...
-    'BackgroundColor', selection_color, 'Callback', @slider_moved);
+    'BackgroundColor', selection_color, 'Callback', @slider_moved_callback);
 text_fs = uicontrol(h, 'Style', 'text', 'String', 'Sampling Rate', ...
     'FontName', 'Helvetica', 'BackgroundColor', figure_color, 'HitTest', 'Off');
 play_button = uicontrol(h, 'Style', 'pushbutton', 'String', 'Play', 'Enable', 'off', 'Callback', @play);
@@ -97,8 +94,7 @@ update_layout;
 
 % show signals
 update_selections([], 'reset');
-time_range_full = [0, max(signal_times)];
-set_time_range_view(time_range_full);
+set_time_range([0, inf]);
 
 % set figure callbacks
 h.CloseRequestFcn = @window_close_callback;
@@ -111,12 +107,12 @@ h.WindowButtonUpFcn = '';
     function update_layout
         h_width = h.Position(3);
         h_height = h.Position(4);
-        hght = (h_height - top_margin - bottom_margin - (num_signals-1) * vert_spacing) / num_signals;
         width = h_width - left_margin - right_margin;
-        if hght > 0 && width > 0
+        height = (h_height - top_margin - bottom_margin - (num_signals-1) * vert_spacing) / num_signals;
+        if height > 0 && width > 0
             for kk = 1:num_signals
-                bottom = bottom_margin + (num_signals - kk) * (hght + vert_spacing);
-                h_ax{kk}.Position = [left_margin, bottom, width, hght];
+                bottom = bottom_margin + (num_signals - kk) * (height + vert_spacing);
+                h_ax{kk}.Position = [left_margin, bottom, width, height];
             end
         end
         play_button.Position = [h_width/2-25, 12, 50, 22];
@@ -150,16 +146,30 @@ h.WindowButtonUpFcn = '';
 
     % plot signals
     function plot_signals
+        delete_patches;   % they wouldn't survive the plotting
         for kk = 1:num_signals
-            t = (1:length(signals{kk})) / fs;
-            signal_times(kk) = t(end);
-            plot(h_ax{kk}, t, signals{kk}, 'HitTest', 'off');
+            t = (1:signal_lengths(kk)) / fs;
+            s = signals{kk};
+            plot(h_ax{kk}, t, s, 'HitTest', 'off');
             h_ax{kk}.UserData = kk;
             h_ax{kk}.Color = selection_color.^(1-selected_axes(kk));
             h_ax{kk}.ButtonDownFcn = @axes_button_down_callback;
             h_ax{kk}.Layer = 'top';
             h_ax{kk}.FontSize = axes_label_font_size;
+            
+            if time_range_view(1) <= signal_lengths(kk) / fs
+                t0 = max(round(time_range_view(1)*fs), 1);
+                t1 = min(round(time_range_view(2)*fs), length(s));
+                maxy = 1.1 * max(max(abs(s(t0:t1, :))));
+                maxy = max(maxy, 1e-9);
+            else
+                maxy = 1;
+            end
+            miny = -maxy * signals_negative(kk);
+            h_ax{kk}.XLim = time_range_view;
+            h_ax{kk}.YLim = [miny, maxy];
         end
+        draw_patches;
     end
 
     function t = get_mouse_pointer_time
@@ -174,37 +184,25 @@ h.WindowButtonUpFcn = '';
         tmouse = min(max(tmouse, time_range_view(1)), time_range_view(2));
         t0 = tmouse - (tmouse - time_range_view(1)) * factor;
         interval = diff(time_range_view) * factor;
-        t0 = max(t0, time_range_full(1));
+        t0 = max(t0, 0);
         t1 = t0 + interval;
-        t1 = min(t1, time_range_full(2));
-        set_time_range_view([t1 - interval, t1]);
+        t1 = min(t1, max(signal_lengths) / fs);
+        set_time_range([t1 - interval, t1]);
     end
 
     % update axis
-    function set_time_range_view(range)
-        min_delta = 1e-4;
-        time_range_view(1) = min(max(range(1), time_range_full(1)), time_range_full(2) - min_delta);
-        time_range_view(2) = max(min(range(2), time_range_full(2)), time_range_full(1) + min_delta);
-        % adjust axis, update y scaling
-        for kk = 1:num_signals
-            s = signals{kk};
-            if time_range_view(1) <= signal_times(kk) && time_range_view(2)*fs >= 1
-                t0 = max(round(time_range_view(1)*fs), 1);
-                t1 = min(round(time_range_view(2)*fs), length(s));
-                maxy = 1.1 * max(max(abs(s(t0:t1, :))));
-                maxy = max(maxy, 1e-9);
-            else
-                maxy = 1;
-            end
-            miny = -maxy * signals_negative(kk);
-            h_ax{kk}.XLim = time_range_view;
-            h_ax{kk}.YLim = [miny, maxy];
-        end
+    function set_time_range(range)
+        min_delta = 10 / fs;
+        max_time = max(signal_lengths) / fs;
+        time_range_view(1) = min(max(range(1), 0), max_time - min_delta);
+        time_range_view(2) = max(min(range(2), max_time), 0 + min_delta);
+        % update signals
+        plot_signals;
         % update slider
         time_diff = diff(time_range_view);
-        frac_time = time_diff / diff(time_range_full);
-        val = (mean(time_range_view) - time_diff / 2 + 1e-6) / (diff(time_range_full) - time_diff + 2e-6);
+        val = (mean(time_range_view) - time_diff / 2 + 1e-6) / (max_time - time_diff + 2e-6);
         time_slider.Value = min(max(val, 0), 1);
+        frac_time = time_diff / max_time;
         if 1
             % zooming out fully, the slider should take up the entire width; but it doesn't
             time_slider.SliderStep = [0.25, 1] * frac_time;
@@ -215,32 +213,46 @@ h.WindowButtonUpFcn = '';
         end
     end
 
+    function delete_patches
+        for kk = 1:num_signals
+            if ~isempty(segment_patches{kk})
+                delete(segment_patches{kk});
+                segment_patches{kk} = [];
+            end
+        end
+    end
+
+    function draw_patches
+        if ~isempty(segment_range)
+            for kk = 1:num_signals
+                if selected_axes(kk)
+                    if isempty(segment_patches{kk})                    
+                        segment_patches{kk} = patch(segment_range([1, 2, 2, 1]), signals_ylim(kk) * [-1, -1, 1, 1], ...
+                            segment_color, 'LineStyle', 'none', 'HitTest', 'off');
+                        uistack(segment_patches{kk}, 'bottom');
+                    else
+                        segment_patches{kk}.Vertices(:, 1) = segment_range([1, 2, 2, 1]);
+                    end
+                end
+            end
+        end
+    end
+
     function axes_button_down_callback(src, ~)
         n_axes = src.UserData;
         disp(['button down on axes ', num2str(n_axes), '; type: ' h.SelectionType]);
         curr_time = get_mouse_pointer_time;
-        % remove segment patch (but remember its range)
-        if ~isempty(segment_patch)
-            segment_range = sort(segment_patch.Vertices(1:2, 1));
-            delete(segment_patch);
-        else
-            segment_range = [];
-        end
-        segment_patch = [];
-        % remove cursor line
-        %         if ~isempty(cursor_line)
-        %             delete(cursor_line);
-        %             cursor_line = [];
-        %         end
         % deal with different types of mouse clicks
+        delete_patches;
+        zoom_range = segment_range;
+        segment_range = [];
         switch(h.SelectionType)
             case 'normal'
                 % left mouse: select current axes; setup segment
                 update_selections(n_axes, 'unique');
+                segment_range = curr_time * [1, 1];
+                draw_patches;
                 % cursor_line = line([1, 1] * curr_time, ylim, 'Color', 'k', 'LineStyle', '--', 'HitTest', 'off');
-                segment_patch = patch(ones(1, 4) * curr_time, signals_ylim(n_axes) * [-1, -1, 1, 1], ...
-                    segment_color, 'LineStyle', 'none', 'HitTest', 'off');
-                uistack(segment_patch, 'bottom');
                 h.WindowButtonUpFcn = @button_up_callback;
                 h.WindowButtonMotionFcn = @button_motion_callback;
             case 'alt'
@@ -252,19 +264,19 @@ h.WindowButtonUpFcn = '';
                     selected_axes(:) = 0;
                     selected_axes(n_axes) = 1;
                     update_selections(n_axes, 'unique');
-                    if isempty(segment_range) || diff(segment_range) == 0
-                        % zoom out 2x around current time
+                    if ~exist('zoom_range', 'var') || isempty(zoom_range) || diff(zoom_range) == 0
+                        % zoom out 2x
                         zoom_axes(2);
                     else
-                        % zoom to selection
-                        set_time_range_view(segment_range);
+                        % zoom to segment
+                        set_time_range(sort(zoom_range));
                     end
                 end
             case 'open'
                 % double click
                 if strcmp(last_button_down, 'normal')
                     % double click left: zoom out full
-                    set_time_range_view(time_range_full);
+                    set_time_range([0, inf]);
                 else
                     % double click 'alt': treat as second of two separate clicks
                     if strcmp(h.CurrentModifier, 'control')
@@ -278,27 +290,25 @@ h.WindowButtonUpFcn = '';
         end
         last_button_down = h.SelectionType;
     end
+
     function button_up_callback(~, ~)
         h.WindowButtonUpFcn = '';
         h.WindowButtonMotionFcn = '';
     end
+
     function button_motion_callback(~, ~)
-        % mouse is dragged across an axes
-        segment_patch.Vertices(2:3,1) = get_mouse_pointer_time;
-        %         if ~isempty(cursor_line)
-        %             delete(cursor_line);
-        %             cursor_line = [];
-        %         end
+        segment_range(2) = get_mouse_pointer_time;
+        draw_patches;
     end
 
     function window_scroll_callback(~, evt)
         zoom_axes(zoom_per_scroll_wheel_step ^ evt.VerticalScrollCount);
     end
 
-    function slider_moved(~, ~)
+    function slider_moved_callback(~, ~)
         time_diff = diff(time_range_view);
-        time_center = 0.5 * time_diff + time_slider.Value * (diff(time_range_full) - time_diff);
-        set_time_range_view(time_center + time_diff * [-0.5, 0.5]);
+        time_center = 0.5 * time_diff + time_slider.Value * (max(signal_lengths) / fs - time_diff);
+        set_time_range(time_center + time_diff * [-0.5, 0.5]);
     end
 
     function play(varargin)
@@ -311,14 +321,18 @@ h.WindowButtonUpFcn = '';
     end
 
     function window_button_down_callback(~, ~)
-        % unselect all axes
+        % mouse click outside axes
         update_selections([], 'reset');
+        delete_patches;
+        segment_range = [];
     end
 
     function change_fs_callback(src, ~)
+        fs_old = fs;
         fs = str2double(src.String{src.Value});
         write_config();
-        plot_signals;
+        segment_range = segment_range * fs_old / fs;
+        set_time_range(time_range_view * fs_old / fs);
     end
 
     function window_resize_callback(~, ~)
