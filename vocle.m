2490b448e5fc57370b2d01fd282a92cbe29812a4
Koen Vos <koenvos@users.noreply.github.com>
2016-04-27 11:01:29 +0800

    updated comments

--- vocle.m
+++ vocle.m
@@ -2,6 +2,18 @@ function vocle(varargin)
 % VOCLE Audio navigator
 %  Vocle lets you view, play and compare audio signals.
 %
+%  Vocle is inspired by Thomas Eriksson's spclab, and shares some of its behavior. 
+%  Advantages over spclab:
+%   - A/B test (select two signals)
+%   - Stereo support
+%   - Possible to interrrupt playback
+%   - Scroll wheel zooming
+%   - Use sampling rate info from input files, if available
+%   - Remember window locations and sampling rate
+%   - Auto update spectrum when highlighting a new segment
+%   - Option to display spectrum on perceptual frequency scale
+%   - Fix some spclab features that broke over time by changes in Matlab
+%
 %  Usage
 %     Vocle([fs,] x, y);         Open vocle with signals x and y, optionally setting the sampling rate to fs 
 %     Vocle('x.wav', 'y.mp3');   Open vocle with files x.wav and y.mp3
@@ -13,24 +25,12 @@ function vocle(varargin)
 %     Left mouse:                Toggle axes selection
 %     Left mouse + drag:         Highlight segment
 %     Right mouse:
-%      - if highlight exists:    Zoom to highlighted segment; remove highlight
-%      - otherwise:              Zoom out
+%      - If highlight exists:    Zoom to highlighted segment; remove highlight
+%      - Otherwise:              Zoom out
 %     Double click left:         Zoom out full
 %     Shift + left/right:        Play
 %     Mouse click outside axes:  Remove highlight
 %     Mouse scroll:              Zoom in or out
-%
-%  Vocle is inspired by Thomas Eriksson's spclab, and shares some of its behavior. 
-%  Advantages over spclab:
-%   - A/B test (select two signals)
-%   - Stereo support
-%   - Possible to stop playback
-%   - Scroll wheel zooming
-%   - Use sampling rate info from input files, if available
-%   - Remember window locations and sampling rate
-%   - Auto update spectrum when highlighting a new segment
-%   - Option to display spectrum on perceptual frequency scale
-%   - Fix some spclab features that were broken by changes in Matlab
 %
 %  Vocle requires Matlab 2014b or newer.
 
